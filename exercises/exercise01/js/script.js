0f239f2985804d284f8381f6de5109abf9cb4aa6
marielitalien <mawiebie@gmail.com>
2017-01-16 16:40:37 -0500

    remove js for not

--- exercises/exercise01/js/script.js
+++ exercises/exercise01/js/script.js
@@ -1,17 +1,1 @@
 confirm('Hello.');
-
-$(document).ready(function() {
-   $('div').mouseenter(function() {
-       $(this).animate({
-           height: '+=10px'
-       });
-   });
-   $('div').mouseleave(function() {
-       $(this).animate({
-           height: '-=10px'
-       });
-   });
-   $('div').click(function() {
-       $(this).toggle(1000);
-   });
-});
