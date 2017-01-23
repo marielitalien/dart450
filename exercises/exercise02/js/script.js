d7d0b9dbea4f557d072a47e494a17fbaf35b2cf0
marielitalien <mawiebie@gmail.com>
2017-01-23 15:49:30 -0500

    asd

---
+++ exercises/exercise02/js/script.js
@@ -0,0 +1,23 @@
+$("#one").click(function(){
+  alert("WHAT");
+});
+
+function changeText(id) {
+    id.innerHTML = "Ooops!";
+}
+
+function changeText2(id) {
+    id.innerHTML = "Nope!";
+}
+//dissapear
+function changeText3(id) {
+    id.innerHTML = "";
+}
+
+function changeText4(id) {
+    id.innerHTML = "Guess not.";
+}
+
+function changeText5(id) {
+    id.innerHTML = "potato";
+}
